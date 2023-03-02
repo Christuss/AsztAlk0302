f297585c8c8bf6b7e47d80fccd42a5aa7b7a0a61
christuss <szendefi.krisztofer@gmail.com>
2023-03-02 14:13:03 +0100

    sarkanyos feladat

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -29,18 +29,27 @@ public class Szamok {
                 }
             }
         } else if (bekeres.contains("nem")) {
-                for (int k = 2; k < 5; k++) {
-                    for (int i = 2; i < 5; i++) {
-                        for (int j = 2; j < 5; j++) {
-                            if (k != i && i != j && k != j) {
-                                System.out.println(k + "" + i + "" + j);
-                                darab++;
-                            }
-
+            for (int k = 2; k < 5; k++) {
+                for (int i = 2; i < 5; i++) {
+                    for (int j = 2; j < 5; j++) {
+                        if (k != i && i != j && k != j) {
+                            System.out.println(k + "" + i + "" + j);
+                            darab++;
                         }
+
                     }
                 }
             }
+        }
         System.out.println("Darab: " + darab);
+
+        for (int j = 1; j < Math.floor(145/11); j++) {
+            for (int i = 1; i < Math.floor(145/7); i++) {
+                if (7 * i + 11 * j == 145) {
+                    System.out.println("hét fejűek: "+i+" \ntizenegy fejűek: "+j);
+                }
+            }
+        }
+
     }
 }
