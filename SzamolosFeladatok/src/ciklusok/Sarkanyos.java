ae67b9523d7626c4e76fc89ba03b446bc0f89ff9
christuss <szendefi.krisztofer@gmail.com>
2023-03-02 14:33:43 +0100

    sarkanyos

--- SzamolosFeladatok/src/ciklusok/Sarkanyos.java
+++ SzamolosFeladatok/src/ciklusok/Sarkanyos.java
@@ -9,5 +9,13 @@ package ciklusok;
  * @author User
  */
 public class Sarkanyos {
-    
+    public static void main(String[] args) {
+        for (int j = 1; j < Math.floor(145 / 11); j++) {
+            for (int i = 1; i < Math.floor(145 / 7); i++) {
+                if (7 * i + 11 * j == 145) {
+                    System.out.println("hét fejűek: " + i + " \ntizenegy fejűek: " + j);
+                }
+            }
+        }
+    }
 }
