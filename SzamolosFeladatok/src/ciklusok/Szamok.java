19d31bff3f9c5454bd7b73bcb65fd1b9ca72b062
christuss <szendefi.krisztofer@gmail.com>
2023-03-02 13:16:38 +0100

    ciklusok feladat

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -9,5 +9,12 @@ package ciklusok;
  * @author User
  */
 public class Szamok {
-    
+    public static void main(String[] args) {
+        for (int i = 2; i < 4; i++) {
+            for (int j = 2; j < 5; j++) {
+            System.out.println(i + "" + j);
+            } 
+        }
+             
+    }
 }
